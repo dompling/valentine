f45c7700971c91f620c1ad9819e50cff5989d059
ShellManager <374779789@qq.com>
2022-08-04 16:01:53 +0800

    测试

--- babel.config.js
+++ babel.config.js
@@ -6,9 +6,7 @@ module.exports = {
   ],
   plugins: [
     ['@babel/plugin-proposal-decorators', {legacy: true}],
-    ['@babel/plugin-proposal-class-properties', {
-      loose: true
-    }],
+    ['@babel/plugin-proposal-class-properties', {}],
     '@babel/plugin-transform-runtime',
     '@babel/plugin-transform-react-jsx',
     'react-hot-loader/babel'
